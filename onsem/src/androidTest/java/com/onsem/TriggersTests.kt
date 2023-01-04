b9af1869cf808fb1732df44fa2ae3933a1c8e152
carloacu <carloacu.dev@gmail.com>
2023-01-04 11:14:02 +0100

    Fix length convertions

--- onsem/src/androidTest/java/com/onsem/TriggersTests.kt
+++ onsem/src/androidTest/java/com/onsem/TriggersTests.kt
@@ -33,18 +33,18 @@ class TriggersTests {
 
         val parameters = HashMap<String, Array<String>>();
         parameters["distance"] = arrayOf(
-            "De combien dois-je avancer en centimètres ?",
-            "De combien dois-je aller vers l'avant en centimètres ?",
-            "De combien dois-je aller tout droit en centimètres ?")
+            "De combien dois-je avancer en mètres ?",
+            "De combien dois-je aller vers l'avant en mètres ?",
+            "De combien dois-je aller tout droit en mètres ?")
 
         addTriggerToAResource("Avance", "mission", "avance-id", parameters, Locale.FRENCH, semanticMemory, linguisticDb)
 
         val textProcessingContext = TextProcessingContext(toRobot = true, Locale.FRENCH)
-        val semExp = textToSemanticExpression("Avance de 2 mètres", textProcessingContext, SemanticSourceEnum.UNKNOWN,
+        val semExp = textToSemanticExpression("Avance de 30 centimètres", textProcessingContext, SemanticSourceEnum.UNKNOWN,
             semanticMemory, linguisticDb)
         val testExecutor = TestExecutor()
         reactFromTrigger(semExp, Locale.FRENCH, semanticMemory, linguisticDb, testExecutor)
-        assertEquals("onResource(mission, avance-id, {distance=200 centimètres})", testExecutor.str)
+        assertEquals("onResource(mission, avance-id, {distance=0,3 mètre})", testExecutor.str)
     }
 
 }
