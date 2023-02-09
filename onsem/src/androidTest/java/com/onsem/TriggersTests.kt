ace79a796a65f05ced1e9ecab3ebc239dbe93c5b
carloacu <carloacu.dev@gmail.com>
2023-02-09 16:49:14 +0100

    Add english tests

--- onsem/src/androidTest/java/com/onsem/TriggersTests.kt
+++ onsem/src/androidTest/java/com/onsem/TriggersTests.kt
@@ -24,12 +24,21 @@ class TriggersTests {
         }
     }
 
+    private fun reactFromTriggerStr(locale: Locale, input: String, semanticMemory: SemanticMemory, linguisticDb: LinguisticDatabase): String {
+        val textProcessingContext = TextProcessingContext(toRobot = true, locale)
+        val semExp = textToSemanticExpression(input, textProcessingContext, SemanticSourceEnum.UNKNOWN,
+            semanticMemory, linguisticDb)
+        val testExecutor = TestExecutor()
+        reactFromTrigger(semExp, locale, semanticMemory, linguisticDb, testExecutor)
+        return testExecutor.str
+    }
 
     @Test
-    fun triggers() {
+    fun triggersFrench() {
         val targetContext: Context = InstrumentationRegistry.getInstrumentation().targetContext
         val semanticMemory = SemanticMemory()
         val linguisticDb = LinguisticDatabase(targetContext.assets)
+        val locale = Locale.FRENCH
 
         val parameters = HashMap<String, Array<String>>();
         parameters["distance"] = arrayOf(
@@ -37,14 +46,23 @@ class TriggersTests {
             "De combien dois-je aller vers l'avant en mètres ?",
             "De combien dois-je aller tout droit en mètres ?")
 
-        addTriggerToAResource("Avance", "mission", "avance-id", parameters, Locale.FRENCH, semanticMemory, linguisticDb)
+        addTriggerToAResource("Avance", "mission", "avance-id", parameters, locale, semanticMemory, linguisticDb)
 
-        val textProcessingContext = TextProcessingContext(toRobot = true, Locale.FRENCH)
-        val semExp = textToSemanticExpression("Avance de 30 centimètres", textProcessingContext, SemanticSourceEnum.UNKNOWN,
-            semanticMemory, linguisticDb)
-        val testExecutor = TestExecutor()
-        reactFromTrigger(semExp, Locale.FRENCH, semanticMemory, linguisticDb, testExecutor)
-        assertEquals("onResource(mission, avance-id, {distance=0,3 mètre})", testExecutor.str)
+        assertEquals("onResource(mission, avance-id, {distance=0,3 mètre})",
+            reactFromTriggerStr(locale, "Avance de 30 centimètres", semanticMemory, linguisticDb))
+    }
+
+    @Test
+    fun triggersEnglish() {
+        val targetContext: Context = InstrumentationRegistry.getInstrumentation().targetContext
+        val semanticMemory = SemanticMemory()
+        val linguisticDb = LinguisticDatabase(targetContext.assets)
+        val locale = Locale.ENGLISH
+
+        addTriggerToAResource("tell me who you are", "mission", "reaction-id", mapOf(), locale, semanticMemory, linguisticDb)
+
+        assertEquals("onResource(mission, reaction-id, {})",
+            reactFromTriggerStr(locale, "tell me who you are", semanticMemory, linguisticDb))
     }
 
 }
